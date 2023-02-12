df7861f41dcc58c3ba9e4e355e2a569c0e4158a9
Androooin <Androooin@yandex.ru>
2023-02-12 20:26:24 +0300

    начало общее

---
+++ dz2/Program.cs
@@ -0,0 +1,56 @@
+﻿//Задача 2: Напишите программу, которая на вход принимает
+//два числа и выдаёт, какое число большее, а какое меньшее.
+
+//a = 5; b = 7 -> max = 7
+//a = 2 b = 10 -> max = 10
+//a = -9 b = -3 -> max = -3
+
+int a;
+int b;
+int max = 0;
+
+a = 5;
+b = 7;
+
+if (a > b)
+{
+    Console.Write("max = ");
+    Console.WriteLine(max = a);
+}
+else
+{
+    Console.Write("max = ");
+    Console.WriteLine(max = b);
+}
+
+
+a = 2;
+b = 10;
+max = 0;
+
+if (a > b)
+{
+    Console.Write("max = ");
+    Console.WriteLine(max = a);
+}
+else
+{
+    Console.Write("max = ");
+    Console.WriteLine(max = b);
+}
+
+
+a = -9;
+b = -3;
+max = 0;
+
+if (a > b)
+{
+    Console.Write("max = ");
+    Console.WriteLine(max = a);
+}
+else
+{
+    Console.Write("max = ");
+    Console.WriteLine(max = b);
+}
---
+++ dz4/Program.cs
@@ -0,0 +1,98 @@
+﻿//Задача 4: Напишите программу, которая принимает на вход три 
+//числа и выдаёт максимальное из этих чисел.
+
+//2, 3, 7 -> 7
+//44 5 78 -> 78
+//22 3 9 -> 22
+
+
+
+int a;
+int b;
+int c;
+int max;
+
+a = 2;
+b = 3;
+c = 7;
+max = 0;
+
+if (a > b)
+{
+   max = a; 
+}
+else 
+{
+max = b;
+}
+if (a > c)
+{
+    max = a;
+}
+else 
+{
+    max = c;
+}
+if (b > c)
+{ 
+    max = b;
+}
+
+Console.WriteLine(max); 
+
+
+a = 44;
+b = 5;
+c = 78;
+max = 0;
+
+if (a > b)
+{
+   max = a; 
+}
+else 
+{
+max = b;
+}
+if (a > c)
+{
+    max = a;
+}
+else 
+{
+    max = c;
+}
+if (b > c)
+{ 
+    max = b;
+}
+
+Console.WriteLine(max); 
+
+a = 22;
+b = 3;
+c = 9;
+max = 0;
+
+if (a > b)
+{
+   max = a; 
+}
+else 
+{
+max = b;
+}
+if (a > c)
+{
+    max = a;
+}
+else 
+{
+    max = c;
+}
+if (b > c)
+{ 
+    max = b;
+}
+
+Console.WriteLine(max); 
